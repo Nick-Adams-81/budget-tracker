b44462de2fa0021b488540e00a41827780bd37f5
nick adams <thebigdeal916@icloud.com>
2020-05-06 15:24:19 -0700

    db.js add

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,7 +9,7 @@ const FILES_TO_CACHE = [
 ]
 
 const CACHE_NAME = 'static-cache-v1';
-const DATA_CACHE_NAME = 'data-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
 
 // install cache
 self.addEventListener('install', function (evt) {
@@ -29,7 +29,7 @@ self.addEventListener('activate', function (evt) {
             return Promise.all(
                 keyList.map(key => {
                     if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log('Remopving old cache data', key);
+                        console.log('Removing old cache data', key);
                         return caches.delete(key)
                     }
                 })
